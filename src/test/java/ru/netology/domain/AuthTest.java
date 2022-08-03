96de4cc4c1c96a51bf29ad2f2680b30ff3398662
Азат Ильдарханов <defcon797@gmail.com>
2022-08-03 21:55:13 +0300

    Reformated code according to the template

--- src/test/java/ru/netology/domain/AuthTest.java
+++ src/test/java/ru/netology/domain/AuthTest.java
@@ -2,10 +2,6 @@ package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.entities.User;
@@ -13,18 +9,8 @@ import ru.netology.domain.utils.DataGenerator;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
 
 public class AuthTest {
-    // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
     @BeforeEach
     void openBrowser() {
         open("http://localhost:9999");
@@ -32,45 +18,20 @@ public class AuthTest {
 
     @Test
     void shouldAddUserAndAuthTest() {
-        User user = DataGenerator.Registration.generateUser("ru", "active");
+        User user = DataGenerator.Registration.registrationUser("active");
         SelenideElement form = $(".form");
-        DataGenerator.printTestData(user);
-
-        // запрос на добавление пользователся в тестовом режиме
-        given() // "дано"
-                // указываем, какую спецификацию используем
-                .spec(requestSpec)
-                // передаём в теле объект, который будет преобразован в JSON
-                .body(user)
-                // "когда"
-                .when()
-                // на какой путь, относительно BaseUri отправляем запрос
-                .post("/api/system/users")
-                // "тогда ожидаем"
-                .then()
-                // код 200 OKx
-                .statusCode(200);
 
         form.$("[data-test-id='login'] input").setValue(user.getLogin());
         form.$("[data-test-id='password'] input").setValue(user.getPassword());
         form.$(".button.button").click();
 
-        $("body [id='root'] .App_appContainer__3jRx1").shouldHave(Condition.text("Личный кабинет"));
+        $(".heading_theme_alfa-on-white").shouldHave(Condition.text("Личный кабинет"));
     }
 
     @Test
     void shouldAddBlockedUserAndAuthTest() {
-        User user = DataGenerator.Registration.generateUser("ru", "blocked");
+        User user = DataGenerator.Registration.registrationUser("blocked");
         SelenideElement form = $(".form");
-        DataGenerator.printTestData(user);
-
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
 
         form.$("[data-test-id='login'] input").setValue(user.getLogin());
         form.$("[data-test-id='password'] input").setValue(user.getPassword());
@@ -83,19 +44,9 @@ public class AuthTest {
 
     @Test
     void shouldAddNoValidateUserAndAuthTest() {
-        User user = DataGenerator.Registration.generateUser("ru", "active");
+        User user = DataGenerator.Registration.registrationUser("active");
         SelenideElement form = $(".form");
-        String noValidateName = "monkey14";
-        DataGenerator.printTestData(user);
-        DataGenerator.printTestData(noValidateName, user.getPassword(), user.getStatus());
-
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
+        String noValidateName = DataGenerator.getRandomPassword();
 
         form.$("[data-test-id='login'] input").setValue(noValidateName);
         form.$("[data-test-id='password'] input").setValue(user.getPassword());
@@ -108,19 +59,9 @@ public class AuthTest {
 
     @Test
     void shouldAddUserNoValidatePasswordAndAuthTest() {
-        User user = DataGenerator.Registration.generateUser("ru", "active");
+        User user = DataGenerator.Registration.registrationUser("active");
         SelenideElement form = $(".form");
-        String noValidatePassword = "monkey14@sdaf1234";
-        DataGenerator.printTestData(user);
-        DataGenerator.printTestData(user.getLogin(), noValidatePassword, user.getStatus());
-
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
+        String noValidatePassword = DataGenerator.getRandomPassword();
 
         form.$("[data-test-id='login'] input").setValue(user.getLogin());
         form.$("[data-test-id='password'] input").setValue(noValidatePassword);
--- src/test/java/ru/netology/domain/utils/DataGenerator.java
+++ src/test/java/ru/netology/domain/utils/DataGenerator.java
@@ -1,40 +1,64 @@
 package ru.netology.domain.utils;
 
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import lombok.experimental.UtilityClass;
 import ru.netology.domain.entities.User;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
 
 @UtilityClass
 public class DataGenerator {
+    // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private static void sendRequest(User user) {
+        // запрос на добавление пользователся в тестовом режиме
+        given() // "дано"
+                // указываем, какую спецификацию используем
+                .spec(requestSpec)
+                // передаём в теле объект, который будет преобразован в JSON
+                .body(user)
+                // "когда"
+                .when()
+                // на какой путь, относительно BaseUri отправляем запрос
+                .post("/api/system/users")
+                // "тогда ожидаем"
+                .then()
+                // код 200 OKx
+                .statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
     @UtilityClass
     public static class Registration {
-        public static User generateUser(String locale, String status) {
-            Faker faker = new Faker(new Locale(locale));
-            return new User(faker.name().username(), faker.internet().password(), status);
+        public static User generateUser(String status) {
+            return new User(getRandomLogin(), getRandomPassword(), status);
         }
-    }
 
-    /**
-     * Метод получения псевдослучайного целого числа от min до max (включая max);
-     */
-    public static int rnd(int min, int max) {
-        max -= min;
-        return (int) (Math.random() * ++max) + min;
-    }
-
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    public void printTestData(String login, String password, String status) {
-        System.out.println("\n" + login + "\n" + password + "\n" + status + "\n");
-    }
-
-    public void printTestData(User user) {
-        printTestData(user.getLogin(), user.getPassword(), user.getStatus());
+        public static User registrationUser(String status) {
+            User registeredUser = generateUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
     }
 }
